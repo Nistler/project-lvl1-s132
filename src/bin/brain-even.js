701dc7ea92ec8b4ac5a94a5ee2100784bf7ee59c
eugene <evgensin@gmail.com>
2017-10-11 16:18:43 +0300

    brain-even game

---
+++ src/bin/brain-even.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import { getName, startEven } from '..';
+
+console.log('Welcome to the Brain Games!');
+console.log('Answer "yes" if number even otherwise answer "no".');
+startEven();
--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 import readlineSync from 'readline-sync';
 
-const getName = () => {
+export const getName = () => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hi, ${name}!`);
+  return name;
 };
-export default getName;
+
+export const startEven = () => {
+  const name = getName(); 
+  let q = null;
+  let a = null;
+  const wrA = (ans) => {
+    const rev = (ans === 'yes') ? 'no' : 'yes';
+    console.log(`'${ans}' is wrong answer :(. Correct answer was '${rev}'.`);
+    console.log(`Let's try again, ${name}!`);
+  };
+  for (let i = 0; i < 3; i += 1) {
+    q = Math.round(1 + (Math.random() * 100));
+    console.log('Question: ', q);
+    a = readlineSync.question('Your answer: ');
+    if (q % 2 === 0 && a !== 'yes') return wrA(a);
+    else if (q % 2 !== 0 && a !== 'no') return wrA(a);
+    console.log('Correct!');
+  }
+  return console.log(`Congratulations, ${name}`);
+};
