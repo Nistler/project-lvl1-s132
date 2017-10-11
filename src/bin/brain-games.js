f31bbb5a827d6a62c101ab53d9fa879c33981b78
eugene <evgensin@gmail.com>
2017-10-11 13:33:44 +0300

    style fix

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
-import {getName} from '..';
-console.log("\x1b[31mWelcome\x1b[0m to the Brain Games!");
+import { getName } from '..';
+
+console.log('\x1b[31mWelcome\x1b[0m to the Brain Games!');
 getName();
