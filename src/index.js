1388401ba3c7f2e3c8951351e088ec531b6cd5e8
eugene <evgensin@gmail.com>
2017-10-14 18:06:47 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import { car, cdr } from 'hexlet-pairs';
 const stepsToWin = 3;
 
 const startGame = (makeTask, gameDescription) => {
-  console.log('\nWelcome to the Brain Games!');
-  console.log(`${gameDescription}\n`);
+  console.log();
+  console.log('Welcome to the Brain Games!');
+  console.log(gameDescription);
+  console.log();
   const username = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${username}!`);
   for (let step = 1; step <= stepsToWin; step += 1) {
     const task = makeTask();
     const question = car(task);
     const correctAnswer = cdr(task);
-    console.log('\nQuestion: ', question);
+    console.log();
+    console.log('Question: ', question);
     const playerAnswer = readlineSync.question('Your answer: ');
     if (correctAnswer !== playerAnswer) {
       console.log(`"${playerAnswer}" is wrong answer :(. Correct answer was "${correctAnswer}".`);
