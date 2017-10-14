bc08b307a1233a222938076c1b50416796d9780f
eugene <evgensin@gmail.com>
2017-10-14 13:23:54 +0300

    add_balance_game

---
+++ src/games/balance.js
@@ -0,0 +1,39 @@
+import { cons } from 'hexlet-pairs';
+import { startGame, RandomNumber } from '..';
+
+const rules = 'Balance the given number.\n';
+const maxNum = 999;
+
+const summing = (number, length) => {
+  let sum = 0;
+  for (let i = 0; i < length; i += 1) {
+    sum += Number(String(number)[i]);
+  }
+  return sum;
+};
+
+// Балансировка числа итерационным процессом
+const iterBalance = (sumOfDigits, length, balanced) => {
+  if (length === 0) return Number(balanced);
+  const digit = Math.floor(sumOfDigits / length);
+  const newSumOfDigits = sumOfDigits - digit;
+  const newLength = length - 1;
+  const newBalanced = String(balanced) + String(digit);
+  return iterBalance(newSumOfDigits, newLength, newBalanced);
+};
+
+// Создание сбалансированного числа
+const makeBalanced = (num) => {
+  const fullLength = String(num).length;
+  const sumOfDig = summing(num, fullLength);
+  return iterBalance(sumOfDig, fullLength, '');
+};
+
+// Генерация задачи
+const makeTask = () => {
+  const question = RandomNumber(maxNum);
+  const answer = makeBalanced(question);
+  return cons(question, `${answer}`);
+};
+
+export default () => startGame(makeTask, rules);
